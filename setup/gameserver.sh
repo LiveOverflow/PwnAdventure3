ac629bfedd428b8e7cf2900f2cf913c2f6548b48
0x6861636b6572 <v.samuel88@yahoo.com>
2018-05-30 00:00:00 -0500

    update for fresh install on debian

--- setup/gameserver.sh
+++ setup/gameserver.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-su pwn3 -c "cd /opt/pwn3/client/PwnAdventure3_Data/PwnAdventure3/PwnAdventure3/Binaries/Linux/ && ./PwnAdventure3Server"
+# it only ran as root for me 
+su root -c "cd /opt/pwn3/client/PwnAdventure3_Data/PwnAdventure3/PwnAdventure3/Binaries/Linux/ && ./PwnAdventure3Server"
